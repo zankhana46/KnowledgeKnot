2f94fa5211e70c595cb56b12e210dee70fdafe7a
zankhana46 <zankhana.mehta02@gmail.com>
2024-04-21 01:00:36 +0530

    hello world

---
+++ frontend/data/footer-links.js
@@ -0,0 +1,27 @@
+export const FooterLink2 = [
+  {
+    title: "Career Paths",
+    links: [
+      { title: "ML Engineer", link: "/al" },
+      { title: "Data Scientist", link: "/cloud-computing" },
+      { title: "Lawyer", link: "/code-foundations" },
+      { title: "Musician", link: "/computer-science" },
+    ],
+  },
+  {
+    title: "Skills",
+    links: [
+      { title: "Music", link: "/c++" },
+      { title: "Coding", link: "/csharp" },
+      { title: "Data analysis", link: "/html-css" },
+    ],
+  },
+  {
+    title: "Subjects",
+    links: [
+        {title: "Python", link: "/career-paths"},
+        {title: "PowerBI", link: "/career-services"},
+        {title: "Excel", link: "/interview-prep"},
+    ]
+  }
+];
---
+++ frontend/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Basis of Python",
+                description : "This course covers the basic Python syntaxes to get started with the language",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Music theory",
+                description : "This course explores the music theory and staff notations",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Excel",
+                description : "This course teaches basic excel and how to do normal data exploration in it",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "Web development",
+                description : "Learn Html, CSS and Java script to kick start your career in web development.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Java",
+                description : "This course gives an elaborated overview of the JAVA language and is best if you are new to the language",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Basis of Python",
+                description : "This course covers the basic Python syntaxes to get started with the language",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Advanced Python",
+                description : "This course explores advanced topics in Python including python libraries for ML and DS",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "SCSS",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Flask",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Django",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Fast API",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Next.js",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Nuxt.js",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Sanity",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
